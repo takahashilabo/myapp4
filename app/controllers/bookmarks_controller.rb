b35af38ac0971495d1f82996d6c44f65e8362635
Keiichi Takahashi <keiichi.takahashi@gmail.com>
2023-07-08 22:40:24 +0900

    ex4

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,6 +3,13 @@ class BookmarksController < ApplicationController
     @bookmarks = Bookmark.all
   end
 
+  def new
+    @bookmark = Bookmark.new
+  end
+
   def create
+    bookmark = Bookmark.new(title: params[:bookmarks][:title], url: params[:bookmarks][:url])
+    bookmark.save
+    redirect_to '/bookmarks/index'
   end
 end

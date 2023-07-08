b92cf3bada5cd99e6790cb029331c3e5373a78a6
Keiichi Takahashi <keiichi.takahashi@gmail.com>
2023-07-08 22:38:54 +0900

    ex3

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,4 +2,7 @@ class BookmarksController < ApplicationController
   def index
     @bookmarks = Bookmark.all
   end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'bookmark/index', to: 'bookmark#index'
-  root 'bookmark#index'
+  get 'bookmarks/index', to: 'bookmarks#index'
+  get 'bookmarks/new', to: 'bookmarks#new'
+  post 'bookmarks/create', to: 'bookmarks#create'
+  root 'bookmarks#index'
 end
